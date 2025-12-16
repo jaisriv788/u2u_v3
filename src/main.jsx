411a5594d979648f50758d83ccfe769f65e28c49
jaisriv788 <srivastavajai845@gmail.com>
2025-12-16 12:14:26 +0530

    done with the changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/stage/">
+    <BrowserRouter basename="/stage/app/">
       <App />
     </BrowserRouter>
   </StrictMode>
